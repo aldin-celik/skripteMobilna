8d697bccbd6ff149d8686bca34d06d4e0c2f3c40
aldin-celik <acelik3@etf.unsa.ba>
2019-04-30 17:22:33 +0200

    skripte

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # skripteMobilna
+
+Za pokretanje skripti je potrebno imati instaliran Andorid Studio sa defaultnom lokacijom SDK-a.
---
+++ pokretanjeAplikacije.bat
@@ -0,0 +1,9 @@
+mkdir mobilna
+cd mobilna
+git clone -b develop https://github.com/si-2019/SI-India-November-2019.git
+cd SI-India-November-2019
+cd MobilnaAplikacija
+call npm install -g expo-cli
+call npm install
+call npm start
+
