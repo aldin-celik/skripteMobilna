a9819d69ab6cbdc317b09a5a84765d103cebdf1e
aldin-celik <49170083+aldin-celik@users.noreply.github.com>
2019-04-30 18:50:48 +0200

    Update pokretanjeEmulatora.bat

--- pokretanjeEmulatora.bat
+++ pokretanjeEmulatora.bat
@@ -1,5 +1,7 @@
 cd c:\Users\%username%\AppData\Local\Android\Sdk\tools\bin
 call sdkmanager --verbose "system-images;android-23;google_apis;x86"
+echo y
 echo no | call avdmanager -v create avd -n test -k "system-images;android-23;google_apis;x86" -g "google_apis" --force
+echo no
 cd ..
 call emulator -avd test
